08fd81a1a80e5af530d127f67797dd02ab61e0b0
Edward Kim <jimmy04500@gmail.com>
2016-12-07 05:39:25 -0600

    ho

---
+++ config/credentials.js
@@ -0,0 +1,4 @@
+module.exports = {
+    connection_string : "mongodb://root:root@ds113668.mlab.com:13668/foodful",
+        secret_key: "B5742DE4959E0C1E5FA207FF6399B18187519E4520229093FE79C5105CDF8834"
+        };
